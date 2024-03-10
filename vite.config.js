f00241c8b7db28157057dc85c24432361aa20dee
muhammed <mokunmoyinbo@gmail.com>
2024-03-10 13:32:44 +0000

    Change

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/asa_frontend_updated/",
+  base: "/asa_frontend_updated-main/",
   plugins: [
     vue(),
     vueJsx(),
